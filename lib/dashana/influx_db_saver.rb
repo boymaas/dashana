834e6077db9bee403b3fcb1efffa36b2b5dadf45
Boy Maas <boy.maas@gmail.com>
2014-11-28 01:29:53 +0100

    enhanced db saver to conform to graphene saving algo

--- lib/dashana/influx_db_saver.rb
+++ lib/dashana/influx_db_saver.rb
@@ -1,4 +1,5 @@
 require 'influxdb'
+require 'base64'
 
 module Dashana
   class InfluxDbSaver
@@ -11,6 +12,10 @@ module Dashana
       @series   = opts.fetch :series
     end
 
+    def series_name(slug) 
+      ("grafana.dashboard_" + Base64.encode64(slug)).chomp
+    end
+
     def influxdb
       @influxdb ||= InfluxDB::Client.new @database,
       host: @host,
@@ -18,12 +23,35 @@ module Dashana
       password: @password
     end
 
+    def slugifyForUrl  str
+        str.downcase.gsub(/[^\w ]+/,'').gsub(/ +/,'-')
+    end
+
+    def saved_dashboards
+      influxdb.query("list series /grafana/")["list_series_result"].map {|e| e["name"]}
+    end
+
+    def drop_all
+      saved_dashboards.each do |name|
+        puts "dropping #{name}"
+        influxdb.query(%Q[drop series "#{name}"])
+      end
+    end
+
+    def drop dash
+      slug = slugifyForUrl(dash.title)
+      influxdb.query(%Q[drop series "#{series_name(slug)}"])
+    end
+
     def save dash
-      influxdb.write_point(@series, {
+      slug = slugifyForUrl(dash.title)
+      influxdb.write_point(series_name(slug), {
                              time: 1000000000,
                              sequence_number: 1,
                              title: dash.title,
-                             dashboard: dash.to_hash
+                             tags: 'dashana',
+                             dashboard: dash.to_hash,
+                             id: slug
                            })
     end
   end
